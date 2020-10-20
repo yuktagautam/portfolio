2e632cc3d4a8347ee44fbb43dd48393300251cf1
yukta gautam <yuktagautam2001@gmail.com>
2020-10-20 22:21:22 +0530

    Updates

---
+++ precache-manifest.907779e437e65cc75f966d0f5403a378.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec28baa6e12d3602c4370cf8d2300d43",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "66e80c527070c9ad4a5f",
+    "url": "/portfolio/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "aa6d1255d70e892dd5f9",
+    "url": "/portfolio/static/css/main.e5bd6c99.chunk.css"
+  },
+  {
+    "revision": "66e80c527070c9ad4a5f",
+    "url": "/portfolio/static/js/2.31738612.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/portfolio/static/js/2.31738612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa6d1255d70e892dd5f9",
+    "url": "/portfolio/static/js/main.4c4ae707.chunk.js"
+  },
+  {
+    "revision": "c749352a3d9bcdc0809d",
+    "url": "/portfolio/static/js/runtime-main.bc1d1cdc.js"
+  },
+  {
+    "revision": "372c4c52072e63fac624d934faaaee8b",
+    "url": "/portfolio/static/media/dsa.372c4c52.jpg"
+  },
+  {
+    "revision": "20887cc7e1171897587d969f31b4d1b8",
+    "url": "/portfolio/static/media/full_stack.20887cc7.png"
+  },
+  {
+    "revision": "1f75707353dee9527cc02c56680c21d2",
+    "url": "/portfolio/static/media/girl_program1.1f757073.png"
+  },
+  {
+    "revision": "4c7702be53762c184e958c35fc3a6591",
+    "url": "/portfolio/static/media/programmer_images.4c7702be.PNG"
+  }
+]);
